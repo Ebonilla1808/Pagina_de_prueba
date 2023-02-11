303d9e5404fa8309805799e98e09f4c8f4b7ae5c
Ebonilla1808 <67129217+Ebonilla1808@users.noreply.github.com>
2023-02-10 22:20:18 -0600

    cargando algunos estilos

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+/* 
+Clase de notificación que informe de un evento al usuario (puede ser incluida en la sección de navegación)
+*/
 class Notificacion{
 
-    Mostrar(){
-        alert("este es un mensaje de notificacion");
+    Mostrar(mensaje){
+        alert(mensaje);
     }
 }
 
